a7d5b7974b10750e223cee8ae0fbdf7391be7663
Paulina Szeszel <paulina_szeszel@o2.pl>
2021-11-29 09:47:06 +0100

    update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import {combineReducers, createStore} from 'redux';
 import initialStoreData from '../data/dataStore';
+import columnsReducer from './columnsRedux';
+import cardsReducer from './cardsRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -7,10 +9,14 @@ const initialState = {
   lists: initialStoreData.lists,
   columns: initialStoreData.columns,
   cards: initialStoreData.cards,
+  //searchString:'',
 };
 
 // define reducers
 const reducers = {
+  columns: columnsReducer,
+  cards: cardsReducer,
+  //searchString: searchReducer,
 };
 
 // add blank reducers for initial state properties without reducers
